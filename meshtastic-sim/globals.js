0fae3582a9c61487eb1ee795a5c613176bf3c1e6
DanielGH3 <danielnazuni@gmail.com>
2025-06-12 07:37:27 +0300

    removed some files

--- meshtastic-sim/globals.js
+++
@@ -1,9 +0,0 @@
-const global = {
-    GRID: {
-        SIZE: 64
-    },
-}
-
-export {
-    global
-}
--- meshtastic-sim/utils.js
+++
@@ -1,9 +0,0 @@
-import menu from "./utils/menu-utils.js";
-import svg from "./utils/svg-utils.js";
-import random from "./utils/random-utils.js";
-
-export default {
-    menu,
-    svg,
-    random
-}
